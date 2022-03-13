8767da56376671deac2462a68066d1a45698c8b7
ss251 <ss251@uw.edu>
2022-03-12 21:43:48 -0600

    added chainlink and unity logos to footer

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -8,6 +8,8 @@ const Footer = () => {
           <img className="ml-2 w-[6.9%] lg:w-[2%] md:w-[4%]" src="/images/Moralis-Icon-Light.png"></img>
           <img className="ml-2 w-[7.2%] lg:w-[2.25%] md:w-[4.25%]" src="/images/ipfs_logo.png"></img>
           <img className="ml-2 w-[15%] lg:w-[4.5%] md:w-[7.5%]" src="/images/pinata_logo.svg"></img>
+          <img className="lg:-ml-10 -ml-8 w-[15.5%] lg:w-[5.7%] md:w-[8%]" src="/images/unity_logo.png"></img>
+          <img className="ml-2 w-[5.8%] lg:w-[1.8%] md:w-[2.8%]" src="/images/chainlink_logo.png"></img>
     </div>
   );
 };
